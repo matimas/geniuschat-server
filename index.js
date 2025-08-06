01743224fc10fe96704bf2d17f4aee475ba1285c
matimas <mati_shak@walla.com>
2025-08-06 23:50:19 +0300

    fix case: cors using

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ const app = express();
 
 require("dotenv").config();
 const mongoose = require('mongoose');
-app.use(cors());
+
+
+/* ☑️ CORS – הרשאות */
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://geniuschat-client.netlify.app"
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    credentials: true          
+  })
+);
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
@@ -14,18 +28,16 @@ mongoose.connect(process.env.MONGO_URI, {
 }).catch(err => {
   console.error("❌ שגיאה בהתחברות ל־MongoDB:", err);
 });
-app.use(cors({ origin: "http://localhost:3000" }));
+
 app.use(express.json());
+
 const chatRoutes = require("./routes/chat"); // הנתיב היחסי
 
 const routes = require("./routes");
 
 app.use("/api", chatRoutes);
 
-
 app.use("/api", routes);
-
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
